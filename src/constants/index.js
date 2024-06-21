9f40f2bffcbcf10df25e811d4d271244f58e32a4
Princeka27 <jpmulyanga@gmail.com>
2024-06-21 02:08:28 +0100

    adding the property section with bad colors

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,115 @@
-
+import apartment from "../assets/images/icon-apartment.png";
+import building from "../assets/images/icon-building.png";
+import house from "../assets/images/icon-house.png";
+import office from "../assets/images/icon-housing.png";
+import luxury from "../assets/images/icon-luxury.png";
+import town from "../assets/images/icon-neighborhood.png";
+import villa from "../assets/images/icon-villa.png";
 export const Menu = ["HOME", "About", "Properties", "Pages", "contact"]
+
+
+export const properties = [
+    {
+        id: 1,
+        title: "Apartment",
+        img:  apartment,
+        location: "New York",
+        bathroom: 2,
+        propertiesquantity: "123 Properties",
+        parking: 2,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, voluptatibus."
+    },
+    {
+        id: 2,
+        title: "Villa",
+        img: villa,
+        propertiesquantity: "123 Properties",
+        price: 200000,
+        location: "New York",
+        bedroom: 3,
+        area: 200,
+        bathroom: 2,
+        parking: 2,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, voluptatibus."
+    },
+    {
+        id: 3,
+        title: "Home",
+        propertiesquantity: "123 Properties",
+        img: house,
+        price: 200000,
+        location: "New York",
+        bedroom: 3,
+        area: 200,
+        bathroom: 2,
+        parking: 2,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, voluptatibus."
+    },
+    {
+        id: 4,
+        title: "Office",
+        propertiesquantity: "123 Properties",
+        img: office,
+        price: 200000,
+        location: "New York",
+        bedroom: 3,
+        area: 200,
+        bathroom: 2,
+        parking: 2,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, voluptatibus."
+    },
+    {
+        id: 5,
+        title: "Building",
+        propertiesquantity: "123 Properties",
+        img: building,
+        price: 200000,
+        location: "New York",
+        bedroom: 3,
+        area: 200,
+        bathroom: 2,
+        parking: 2,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, voluptatibus."
+    },
+    {
+        id: 6,
+        title: "Townhouse",
+        propertiesquantity: "123 Properties",
+        img: town,
+        price: 200000,
+        location: "New York",
+        bedroom: 3,
+        area: 200,
+        bathroom: 2,
+        parking: 2,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, voluptatibus."
+    },
+    {
+        id: 7,
+        title: "Shop",
+        propertiesquantity: "123 Properties",
+        img: town,
+        price: 200000,
+        location: "New York",
+        bedroom: 3,
+        area: 200,
+        bathroom: 2,
+        parking: 2,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, voluptatibus."
+    },
+    {
+        id: 8,
+        title: "Garage",
+        propertiesquantity: "123 Properties",
+        img: luxury,
+        price: 200000,
+        location: "New York",
+        bedroom: 3,
+        area: 200,
+        bathroom: 2,
+        parking: 2,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, voluptatibus."
+    },
+    
+
+]
--- src/sections/Property.jsx
+++ src/sections/Property.jsx
@@ -1,15 +1,37 @@
+import { properties } from "../constants";
 
 const Property = () => {
   return (
     <div className="pt-24">
         <div className=" container">
-            <div className="text-center mx-auto mb-5 animate-fadeIn h-[600px]">
+            <div className="text-center mx-auto animate-fadeIn h-[600px]">
                 <h1 className="mb-3 text-[34px] font-semibold">
                 Property Types
                 </h1>
-                <p className="text-[16px] w-4/5 mx-auto">Eirmod sed ipsum dolor sit rebum labore magna erat. Tempor ut dolore lorem kasd
+                <p className="text-[16px] lg:text-xl w-4/5 mx-auto lg:w-1/2 mb-5">Eirmod sed ipsum dolor sit rebum labore magna erat. Tempor ut dolore lorem kasd
                  vero ipsum sit eirmod sit. Ipsum diam justo sed rebum vero dolor duo.</p>
+            <div className="grid lg:grid-cols-4 gap-4 px-5 pt-10 rounded-sm hover:bg-white">
+                    {
+                        properties.map((el)=>{
+                            return(
+                                <div key={el.id} className="flex flex-col items-center bg-indigo-700 py-5 rounded-lg">
+                                    <div className="w-4/5">
+                                    <div className="bg-red-500 py-5 rounded-lg">
+                                    <div className=" rounded-full h-20 w-20 bg-slate-100 
+                                    border-dashed border-2 border-green-600 relative mx-auto">
+                                    <img src={el.img} alt={el.name} className=" w-11 h-11 mx-auto inset-y-0 absolute inset-x-0 top-3"/>
+                                    </div>
+                                    <h4 className="mt-2 text-center font-semibold">{el.title}</h4>
+                                    <p className="mt-2 text-center font-medium">{el.propertiesquantity}</p>
+                                    </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
             </div>
+            </div>
+            
         </div>
     </div>
   )
